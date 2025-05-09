e0e58473c2257f879c679a4cf5a1b505bd6a05e3
DemiusLs <dloss320@gmail.com>
2025-05-09 20:02:36 +0200

    feat: bonus done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 const emailCont = document.querySelector(".list-group");
 const button = document.querySelector(".btn");
+const spinner = document.querySelector(".spinner-border")
 
 let giri = 10
+let mailDoing = false
 
+let count = 0;
 
 callMail = () => {
+    let mail = "";
+    spinner.classList.remove("d-none")
+    emailCont.innerHTML = null;
+    
 
-    let mail = "null";
+        for (let i = 0; i < giri; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    mail += `<li class="list-group-item"><a href="" type="mail">${resp.data.response}</a></li>`;
 
-    for (let i = 0; i < giri - 1; i++) {
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((resp) => {
-                emailCont.innerHTML += `<li class="list-group-item"><a href="" type="mail">${resp.data.response}</a></li>`;
+                    if (count === 9) {
+                        emailCont.innerHTML = mail;
+                        count = 0;
+                        mailDoing = false;
+                        spinner.classList.add("d-none")
+                    } else {
+                        count++
+                    }
 
+                });
+        }
 
-            });
-    }
-
+   
 
 
 }
+
+
+
+
 
 button.addEventListener("click", callMail)
 
